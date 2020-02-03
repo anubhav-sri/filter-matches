bf3385f62501f4152c21e0cf0e7ffe6a79171941
anubhavsrivastava <anubhav.srivastava@n26.com>
2020-02-03 01:29:02 +0100

    pulls out a test util for builders

--- src/test/java/com/spark/anubhav/services/MatchServiceTest.java
+++ src/test/java/com/spark/anubhav/services/MatchServiceTest.java
@@ -1,6 +1,5 @@
 package com.spark.anubhav.services;
 
-import com.spark.anubhav.models.City;
 import com.spark.anubhav.models.Match;
 import com.spark.anubhav.repositories.MatchRepository;
 import org.junit.jupiter.api.Test;
@@ -8,11 +7,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
+import static com.spark.anubhav.utils.TestUtils.buildMatch;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
@@ -28,24 +27,6 @@ class MatchServiceTest {
         when(repository.findAllByUserId(userId)).thenReturn(Collections.singletonList(aMatch));
         List<Match> matches = new MatchService(repository).findAllMatchesForUser(userId);
         assertThat(matches).containsExactly(aMatch);
-    }
-
-    private Match buildMatch(UUID userId) {
-        return Match.builder()
-                .id(UUID.randomUUID())
-                .age(34)
-                .city(new City("cityName", 78.8, 79.9))
-                .compatibilityScore(BigDecimal.valueOf(54))
-                .height(170)
-                .displayName("Cool Name")
-                .favourite(false)
-                .name("Name")
-                .jobTitle("Job1")
-                .mainPhoto("")
-                .numberOfContactsExchanged(0)
-                .religion("Athiest")
-                .userId(userId)
-                .build();
     }
 
 }
--- src/test/java/com/spark/anubhav/utils/TestUtils.java
+++ src/test/java/com/spark/anubhav/utils/TestUtils.java
@@ -1,4 +1,46 @@
 package com.spark.anubhav.utils;
 
+import com.spark.anubhav.models.City;
+import com.spark.anubhav.models.Match;
+import com.spark.anubhav.models.MatchDTO;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
 public class TestUtils {
+
+    public static Match buildMatch(UUID userId) {
+        return Match.builder()
+                .id(UUID.randomUUID())
+                .age(34)
+                .city(new City("cityName", 78.8, 79.9))
+                .compatibilityScore(BigDecimal.valueOf(54))
+                .height(170)
+                .displayName("Cool Name")
+                .favourite(false)
+                .name("Name")
+                .jobTitle("Job1")
+                .mainPhoto("")
+                .numberOfContactsExchanged(0)
+                .religion("Athiest")
+                .userId(userId)
+                .build();
+    }
+
+    public static MatchDTO buildMatchDTO(Match match) {
+        return MatchDTO.builder()
+                .name(match.getName())
+                .displayName(match.getDisplayName())
+                .age(match.getAge())
+                .city(match.getCity())
+                .compatibilityScore(match.getCompatibilityScore())
+                .favourite(match.getFavourite())
+                .height(match.getHeight())
+                .id(match.getId())
+                .jobTitle(match.getJobTitle())
+                .mainPhoto(match.getMainPhoto())
+                .contactsExchanged(match.getNumberOfContactsExchanged())
+                .religion(match.getReligion())
+                .build();
+    }
 }
